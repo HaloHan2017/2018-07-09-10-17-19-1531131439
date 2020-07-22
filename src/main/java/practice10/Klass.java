c7f6d8da5ebb5df81f932d00e1647882e9d64d4b
HaloHan2017 <1006653740@qq.com>
2020-07-22 23:17:12 +0800

    [practice10] [Jerry] feature : add classes list,isTeaching method and update introduce in Teacher , add isIn method in Klass

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,62 @@
 package practice10;
 
 public class Klass {
+    private int number;
+    private String displayName;
+    private Student leader;
+
+    public Klass(int number, Student leader) {
+        this.number = number;
+        this.leader = leader;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+        this.displayName = "Class " + number;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass() == null || this.number != student.getKlass().getNumber()) {
+            System.out.print("It is not one of us.\n");
+        } else {
+            this.leader = student;
+            appendMember(student);
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student) {
+        Klass newClass = new Klass(this.number, this.leader);
+        student.setKlass(newClass);
+    }
+
+    public boolean isIn(Student student){
+        if(student.getKlass() == null){
+            return false;
+        }
+        return this.number == student.getKlass().getNumber();
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,62 @@
 package practice10;
 
-public class Teacher {
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person {
+    private Collection<Klass> classes;
+
+    public String introduce() {
+        StringBuilder introduce = new StringBuilder(super.introduce());
+        introduce.append(" I am a Teacher. ");
+        if (classes == null || classes.isEmpty()) {
+            introduce.append("I teach No Class.");
+        } else {
+            List<Integer> numbers = classes.stream().map(Klass::getNumber).collect(Collectors.toList());
+            introduce.append(String.format("I teach Class %s.", getFormatNumberStr(numbers)));
+        }
+        return introduce.toString();
+    }
+
+    private String getFormatNumberStr(List<Integer> numbers) {
+        StringBuilder result = new StringBuilder();
+        for (Integer number : numbers) {
+            result.append(number);
+            result.append(", ");
+        }
+        return result.toString().substring(0, result.length() - 2);
+    }
+
+    public String introduceWith(Student student) {
+        String str = "I don't teach %s.";
+        if (isTeaching(student)) {
+            str = "I teach %s.";
+        }
+        return super.introduce() + String.format(" I am a Teacher. " + str, student.getName());
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Collection<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public Collection<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(Collection<Klass> classes) {
+        this.classes = classes;
+    }
+
+    public boolean isTeaching(Student student) {
+        if (student.getKlass() == null) {
+            return false;
+        }
+        return classes.contains(student.getKlass());
+    }
 }
