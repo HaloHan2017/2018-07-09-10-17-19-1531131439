764b35add0aaf84cde84a3d2bb7b050ce8f559d6
HaloHan2017 <1006653740@qq.com>
2020-07-22 21:24:35 +0800

    [practice09] [Jerry] feature : update equals method in Person, assignLeader and appendMember method in Klass

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,31 @@
 package practice09;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public String introduce() {
+        String str = "I am at Class %s.";
+        if (klass != null && klass.getLeader() != null && (this.id == klass.getLeader().getId())) {
+            str = "I am Leader of Class %s.";
+        }
+        return super.introduce() + String.format(" I am a Student. " + str, klass.getNumber());
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
 }
