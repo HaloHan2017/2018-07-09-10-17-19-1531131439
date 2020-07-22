473d9097f211831a57e78cc9ea2b5bc0bebab06d
HaloHan2017 <2332722908@qq.com>
2020-07-22 17:39:35 +0800

    [practice06] [Jerry] feature: add klass and "introduce" method in Teacher

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,35 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    protected Integer klass;
+
+    public String introduce() {
+        StringBuilder introduce = new StringBuilder(super.introduce());
+        introduce.append(" I am a Teacher. ");
+        if(klass == null){
+            introduce.append("I teach No Class.");
+        }else {
+            introduce.append("I teach Class ");
+            introduce.append(klass);
+            introduce.append(".");
+        }
+        return introduce.toString();
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
 }
